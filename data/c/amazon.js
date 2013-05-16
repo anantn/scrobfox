f638b88f465a461c7fc5994595689a9f5923ba3a
Anant Narayanan <anant@kix.in>
2013-05-16 13:32:45 -0700

    Fix whitespace (2 space tabs consistent)

--- data/c/amazon.js
+++ data/c/amazon.js
@@ -1,37 +1,36 @@
 
 function sendCurrentSong() {
-    var song = {};
-    var details = unsafeWindow.amznMusic.widgets.player.getCurrent();
+  var song = {};
+  var details = unsafeWindow.amznMusic.widgets.player.getCurrent();
 
-    if (!details || !details.metadata.title || !details.metadata.artistName) {
-      return;
-    }
+  if (!details || !details.metadata.title || !details.metadata.artistName) {
+    return;
+  }
 
-    var song = {
-      title: details.metadata.title, artist: details.metadata.artistName
-    };
-    if (details.metadata.albumName) {
-      song.album = details.metadata.albumName;
-    }
-    self.postMessage(song);
+  var song = {
+    title: details.metadata.title, artist: details.metadata.artistName
+  };
+  if (details.metadata.albumName) {
+    song.album = details.metadata.albumName;
+  }
+  self.postMessage(song);
 }
 
 var timeout = 2000;
 function checkPlayer() {
-    if (!('amznMusic' in unsafeWindow) ||
-        !unsafeWindow.amznMusic.widgets.player.created) {
-      timeout *= 2;
-      setTimeout(checkPlayer, timeout);
-      return;
-    }
+  if (!('amznMusic' in unsafeWindow) || !unsafeWindow.amznMusic.widgets.player.created) {
+    timeout *= 2;
+    setTimeout(checkPlayer, timeout);
+    return;
+  }
 
-    var oldCallback = unsafeWindow.amznMusic.widgets.player.callback;
-    unsafeWindow.amznMusic.widgets.player.callback = function(e) {
-      if (e == "onPlay") {
-        sendCurrentSong();
-      }
-      oldCallback(e);
+  var oldCallback = unsafeWindow.amznMusic.widgets.player.callback;
+  unsafeWindow.amznMusic.widgets.player.callback = function(e) {
+    if (e == "onPlay") {
+      sendCurrentSong();
     }
+    oldCallback(e);
+  }
 }
 
 checkPlayer();
--- data/panel.js
+++ data/panel.js
@@ -1,42 +1,42 @@
 
 self.port.on("playing", function(song) {
-    var sinfo = document.getElementById("info");
-    sinfo.class = "songinfo";
-    sinfo.innerHTML = '<span class="title">' + song.title + '</span><br/>';
-    sinfo.innerHTML += '<span class="artist">by ' + song.artist;
+  var sinfo = document.getElementById("info");
+  sinfo.class = "songinfo";
+  sinfo.innerHTML = '<span class="title">' + song.title + '</span><br/>';
+  sinfo.innerHTML += '<span class="artist">by ' + song.artist;
 });
 
 self.port.on("connection", function(type) {
-    switch (type) {
-    case "connect":
-        var but = document.getElementById("connector");
-        but.onclick = function() {
-            but.disabled = true;
-            self.port.emit("connect");
-        }
-        break;
-
-    case "failed":
-        // edit connect message and stay at <div id="connect">
-        var but = document.getElementById("connector");
-        but.disabled = false;
-        var mesg = document.getElementById("connector-message");
-        mesg.innerHTML = "Could not authenticate - Try again?";
-        break;
-
-    default:
-        // show <div id="default"> and append name
-        document.getElementById("connect").style.display = "none";
-        document.getElementById("default").style.display = "block";
-        document.getElementById("pandora").onclick = function() {
-            self.port.emit("launch", "pandora");
-        };
-        document.getElementById("amazon").onclick = function() {
-            self.port.emit("launch", "amazon");
-        };
-        var name = document.getElementById("connected");
-        name.innerHTML += type;
-        break;
+  switch (type) {
+  case "connect":
+    var but = document.getElementById("connector");
+    but.onclick = function() {
+      but.disabled = true;
+      self.port.emit("connect");
     }
+    break;
+
+  case "failed":
+    // edit connect message and stay at <div id="connect">
+    var but = document.getElementById("connector");
+    but.disabled = false;
+    var mesg = document.getElementById("connector-message");
+    mesg.innerHTML = "Could not authenticate - Try again?";
+    break;
+
+  default:
+    // show <div id="default"> and append name
+    document.getElementById("connect").style.display = "none";
+    document.getElementById("default").style.display = "block";
+    document.getElementById("pandora").onclick = function() {
+      self.port.emit("launch", "pandora");
+    };
+    document.getElementById("amazon").onclick = function() {
+      self.port.emit("launch", "amazon");
+    };
+    var name = document.getElementById("connected");
+    name.innerHTML += type;
+    break;
+  }
 });
 
